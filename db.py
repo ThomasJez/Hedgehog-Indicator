23032bc7c89e0b98ea7a55bc79761e02e6da921a
Thomas Jez <thomas.jez@gmx.de>
2023-09-05 11:35:02 +0200

    variable names improved, comment added

--- db.py
+++ db.py
@@ -4,6 +4,7 @@ import datetime
 
 
 def get_label():
+    # we don't handle exceptions, because the system exceptions tell everything what's needed if something goes wrong
     conn = sqlite3.connect(config.db_path)
     c = conn.cursor()
     c.execute('SELECT activity_id, start_time, end_time, name FROM facts \
@@ -11,17 +12,17 @@ def get_label():
         ORDER BY start_time DESC \
         LIMIT 1')
     row = c.fetchone()
-    if row == None:
+    if row is None:
         raise Exception('Handling of completely empty rows isn\'t implemented yet')
-    if row[2] == None:
+    if row[2] is None:
         begin = datetime.datetime.strptime(row[1], "%Y-%m-%d %H:%M:%S")
         now = datetime.datetime.now()
         duration = str(now - begin)
         pos = duration.find('.')
         seconds = duration[:pos]
-        taetigkeit = row[3]
-        anzeige = taetigkeit + ' ' + seconds
+        activity = row[3]
+        labeltext = activity + ' ' + seconds
     else:
-        anzeige = 'No Activity'
+        labeltext = 'No Activity'
     conn.close()
-    return anzeige
+    return labeltext
